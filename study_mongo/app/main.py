bba93b2d30ee33a3c240bedf87d82b238c9a0369
vladmialkin <myalkin.vlad@yandex.ru>
2024-10-18 15:22:06 +0300

    feat: Добавление функций для чтения из бд.

--- study_mongo/app/main.py
+++ study_mongo/app/main.py
@@ -1,10 +1,11 @@
+import functools
 import logging
 import random
 import time
 import asyncio
 from logging.handlers import RotatingFileHandler
 from multiprocessing import Process
-from uuid import uuid4, uuid1
+from uuid import uuid4, uuid1, UUID
 
 from beanie import init_beanie
 from bson import Binary
@@ -17,7 +18,7 @@ fake = Faker()
 
 semaphore = asyncio.Semaphore(5)
 
-logger = logging.getLogger('etl_application')
+logger = logging.getLogger('load_data_mongodb')
 logger.setLevel(logging.INFO)
 
 fh = RotatingFileHandler('file.log', maxBytes=20_000_000, backupCount=5)
@@ -26,6 +27,43 @@ formatter = logging.Formatter(
 )
 fh.setFormatter(formatter)
 logger.addHandler(fh)
+
+
+def timeit(func):
+    @functools.wraps(func)
+    async def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = await func(*args, **kwargs)
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        logger.info(f"Функция {func.__name__} выполнена за {elapsed_time:.4f} секунд")
+        return result
+
+    return wrapper
+
+
+@timeit
+async def get_rating_user(user_id: UUID):
+    value = await FilmRating.find(FilmRating.user_id == user_id).to_list()
+    logger.info(f"{value}")
+
+
+@timeit
+async def get_rating_film(rating: int):
+    value = await FilmRating.find(FilmRating.number == rating).to_list()
+    logger.info(f"Количество полученных элементов: {len(value)}")
+
+
+@timeit
+async def get_bookmarks_list():
+    value = await FilmRating.find().to_list()
+    logger.info(f"Количество полученных элементов: {len(value)}")
+
+
+@timeit
+async def get_avg_rating(film_id: UUID):
+    value = await FilmRating.find(FilmRating.film_id == film_id).avg(FilmRating.number)
+    logger.info(f"Средний рейтинг фильма: {value}")
 
 
 async def insert_records(batch_size, count):
@@ -64,6 +102,17 @@ async def insert_records(batch_size, count):
         logger.info(f"Добавлено ~{count} данных")
 
 
+async def insert_to_db(total_records, batch_size):
+    tasks = []
+    logger.info("Начало загрузки данных")
+    start_time = time.time()
+    for count in range(total_records // batch_size):
+        tasks.append(insert_records(batch_size, count * batch_size))
+
+    await asyncio.gather(*tasks)
+    logger.info(f"Затраченное время {(time.time() - start_time) / 60} минут")
+
+
 async def main(total_records, batch_size):
     client = AsyncIOMotorClient('mongodb://localhost:27017', uuidRepresentation="standard")
     await init_beanie(
@@ -71,27 +120,28 @@ async def main(total_records, batch_size):
         document_models=[FilmBookmarks, FilmReviews, FilmRating],
     )
 
+    await insert_to_db(total_records, batch_size)
+
     tasks = []
 
-    start_time = time.time()
-    logger.info("Начало загрузки данных")
-    for count in range(total_records // batch_size):
-        tasks.append(insert_records(batch_size, count * batch_size))
-
+    tasks.append(get_rating_user(user_id=UUID("f1fa9b48-8d33-11ef-a341-7c70db5559dd")))
     await asyncio.gather(*tasks)
 
-    end_time = time.time()
-    logger.info(f"Затраченное время {(end_time - start_time) / 60} минут")
+    tasks.append(get_rating_film(rating=4))
+    await asyncio.gather(*tasks)
+
+    tasks.append(get_bookmarks_list())
+    await asyncio.gather(*tasks)
+
+    tasks.append(get_avg_rating(film_id=UUID("f1fa9bb6-8d33-11ef-a341-7c70db5559dd")))
+    await asyncio.gather(*tasks)
 
 
 def run_async_process(total_records: int, batch_size: int):
     asyncio.run(main(total_records, batch_size))
 
 
-if __name__ == "__main__":
-    total_records = 10_000_000
-    batch_size = 5000
-    num_process = 5
+def run_multiprocessing(total_records, batch_size, num_process):
     processes = []
     for _ in range(num_process):
         process = Process(target=run_async_process, args=(total_records // num_process, batch_size))
@@ -100,3 +150,11 @@ if __name__ == "__main__":
 
     for p in processes:
         p.join()
+
+
+if __name__ == "__main__":
+    total_records = 10_000_000
+    batch_size = 5000
+    num_process = 5
+    run_async_process(total_records, batch_size)
+    # run_multiprocessing(total_records, batch_size, num_process)
